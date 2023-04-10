44e44c1fccc853c990d4a5a1e5b275dbb4b08327
ChatGTHB <cihat.kose@hotmail.com>
2023-04-10 11:55:50 +0300

    The "Finding Horoscopes" project has added.

---
+++ src/_12_FindingHoroscopes/FindingHoroscopes.java
@@ -0,0 +1,154 @@
+package _12_FindingHoroscopes;
+
+import java.util.Scanner;
+
+public class FindingHoroscopes {
+    public static void main(String[] args) {
+
+        String month;
+        int day;
+        boolean isError = false;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the month you were born :");
+        month = input.nextLine();
+
+        System.out.print("Enter the day you were born : ");
+        day = input.nextInt();
+
+
+        if (month.equalsIgnoreCase("January")) {
+            if (day >= 1 && day <= 31) {
+                if (day < 21) {
+                    System.out.println("Capricorn");
+                } else {
+                    System.out.println("Aquarius");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("February")) {
+            if (day >= 1 && day <= 29) {
+                if (day < 20) {
+                    System.out.println("Aquarius");
+                } else {
+                    System.out.println("Pisces");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("March")) {
+            if (day >= 1 && day <= 31) {
+                if (day < 21) {
+                    System.out.println("Pisces");
+                } else {
+                    System.out.println("Aries");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("April")) {
+            if (day >= 1 && day <= 30) {
+                if (day < 21) {
+                    System.out.println("Aries");
+                } else {
+                    System.out.println("Taurus");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("May")) {
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Taurus");
+                } else {
+                    System.out.println("Gemini");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("June")) {
+            if (day >= 1 && day <= 30) {
+                if (day < 22) {
+                    System.out.println("Gemini");
+                } else {
+                    System.out.println("Cancer");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("July")) {
+            if (day >= 1 && day <= 31) {
+                if (day < 23) {
+                    System.out.println("Cancer");
+                } else {
+                    System.out.println("Leo");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("August")) {
+            if (day >= 1 && day <= 31) {
+                if (day < 22) {
+                    System.out.println("Leo");
+                } else {
+                    System.out.println("Virgo");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("September")) {
+            if (day >= 1 && day <= 30) {
+                if (day < 24) {
+                    System.out.println("Virgo");
+                } else {
+                    System.out.println("Libra");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("Oktober")) {
+            if (day >= 1 && day <= 31) {
+                if (day < 24) {
+                    System.out.println("Libra");
+                } else {
+                    System.out.println("Scorpio");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("November")) {
+            if (day >= 1 && day <= 30) {
+                if (day < 23) {
+                    System.out.println("Scorpio");
+                } else {
+                    System.out.println("Sagittarius");
+                }
+            } else {
+                isError = true;
+            }
+        } else if (month.equalsIgnoreCase("December")) {
+            if (day >= 1 && day <= 30) {
+                if (day < 21) {
+                    System.out.println("Sagittarius");
+                } else {
+                    System.out.println("Pisces");
+                }
+            } else {
+                isError = true;
+            }
+        } else {
+            System.out.println("You entered incorrectly, enter the month you were born.");
+        }
+
+        if (isError) {
+            System.out.println("You entered incorrectly, There is no number of days you entered in the month you were born.");
+        }
+    }
+}
+
+
+
+
+
---
+++ src/_12_FindingHoroscopes/README.md
@@ -0,0 +1,10 @@
+# PatikaJavaProject12
+```
+Horoscope Finder Program
+```
+
+```
+Burç Bulan Program
+```
+[Patika](https://www.patika.dev)
+
