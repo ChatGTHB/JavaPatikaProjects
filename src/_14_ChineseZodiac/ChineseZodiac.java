d70710a5c69fdf8bed79c46436aa32e42f741b00
ChatGTHB <cihat.kose@hotmail.com>
2023-04-11 22:38:25 +0300

    The ChineseZodiac project has added.

---
+++ src/_14_ChineseZodiac/ChineseZodiac.java
@@ -0,0 +1,77 @@
+package _14_ChineseZodiac;
+
+import java.util.Scanner;
+
+public class ChineseZodiac {
+    public static void main(String[] args) {
+
+        int birthYear, remainder;
+        String zodiac = null;
+
+        boolean isError = false;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your year of birth : ");
+        birthYear = input.nextInt();
+
+        remainder = birthYear % 12;
+
+        switch (remainder) {
+            case 0: {
+                zodiac = "Monkey";
+                break;
+            }
+            case 1: {
+                zodiac = "Rooster";
+                break;
+            }
+            case 2: {
+                zodiac = "Dog";
+                break;
+            }
+            case 3: {
+                zodiac = "Pig";
+                break;
+            }
+            case 4: {
+                zodiac = "Mouse";
+                break;
+            }
+            case 5: {
+                zodiac = "Ox";
+                break;
+            }
+            case 6: {
+                zodiac = "Tiger";
+                break;
+            }
+            case 7: {
+                zodiac = "Rabbit";
+                break;
+            }
+            case 8: {
+                zodiac = "Dragon";
+                break;
+            }
+            case 9: {
+                zodiac = "Snake";
+                break;
+            }
+            case 10: {
+                zodiac = "Horse";
+                break;
+            }
+            case 11: {
+                zodiac = "Sheep";
+                break;
+            }
+        }
+        System.out.println("Your Chinese Zodiac Horoscope : "+zodiac);
+    }
+}
+
+
+
+
+
---
+++ src/_14_ChineseZodiac/README.md
@@ -0,0 +1,10 @@
+# PatikaJavaProject14
+```
+Calculating Chinese Zodiac value by taking birth date from user with Java.
+```
+
+```
+Java ile kullanıcıdan doğum tarihini alıp Çin Zodyağı değerini hesaplama.
+```
+[Patika](https://www.patika.dev)
+
